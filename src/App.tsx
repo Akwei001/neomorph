05c4c5aadb9ff419be15da253beeba9cb8ffb121
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2026-03-22 12:51:31 +0000

    class variance authority

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import Button from "./components/Button"
+
+import Button from "./components/Button";
+
 
 
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,39 @@
+ import { mergeProps, useRender } from "@base-ui/react";
+ import {cva} from 'class-variance-authority'
+ 
+
+const buttonVariants = cva("inline-flex justify-center items-center bg-red-200", 
+{
+  variants:{
+    variant:{
+      default:"",
+      primary:"",
+      destructive:"",
+      warning:"",
+    },
+    size:{
+      default: "h-12 px-4",
+      icon: "h-12 w-12",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+ 
  export default function Button(props) {
-    const {children, ...otherProps} = props;
-    return <button {...otherProps}>{children}</button>
+   
+
+   const mergedProps = mergeProps(props, {
+      className: buttonVariants(),
+   });
+
+   const element = useRender({
+      defaultTagName: 'button',
+      render: props.render,
+      props: mergedProps,
+   })
+   
+ return element;
  }
